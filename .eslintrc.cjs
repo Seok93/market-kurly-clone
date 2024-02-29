c7c8350158c4fb73dcea7e9822da9a2fcbb584b9
Seok93 <gkdltjr@naver.com>
2024-02-29 13:04:18 +0900

    config: eslint, prettier 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,34 @@
+const vitest = require('eslint-plugin-vitest');
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:vitest/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-hooks/exhaustive-deps': [
       'warn',
-      { allowConstantExport: true },
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
     ],
+    'no-unused-vars': 'warn',
+    'vitest/expect-expect': 'off',
+    'react/prop-types': 'off',
   },
-}
+  globals: {
+    ...vitest.environments.env.globals,
+  },
+};
