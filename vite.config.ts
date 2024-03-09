043100eecc1f832a18dfb4ab1a5220a63ba7e9b0
Seok93 <gkdltjr@naver.com>
2024-03-09 12:06:15 +0900

    config: #9 - vite, ts 절대 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@recoil', replacement: path.resolve(__dirname, 'src/recoil') },
+      { find: '@stories', replacement: path.resolve(__dirname, 'src/stories') },
+      { find: '@contexts', replacement: path.resolve(__dirname, 'src/contexts') },
+      { find: '@tests', replacement: path.resolve(__dirname, 'src/tests') },
+    ]
+  }
 });
